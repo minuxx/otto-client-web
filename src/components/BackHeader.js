dbb4d47ca407036377facc6eaa680ae0a96f5d6f
minuxx <minux8905@gmail.com>
2022-07-15 15:11:59 +0900

    init: 라이딩정보 입력 view

---
+++ src/components/BackHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import leftArrow from '../image/left-arrow.png'
+
+function BackHeader() {
+    return (
+        <img src={leftArrow}/>
+    )
+}
+
+export default BackHeader
---
+++ src/views/RidingInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import BackHeader from '../components/BackHeader'
+
+
+function RidingInfo() {
+
+    return <>
+        <BackHeader />
+    </>
+}
+
+export default RidingInfo
