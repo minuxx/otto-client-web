84609e41a1ab62960fc4659abfc720da36fe737f
지오현 <jihkyuoo@naver.com>
2022-07-20 14:47:35 +0900

    이미지 업로드 가이드 페이지 수정 & 마지막 페이지 리팩토링

---
+++ src/components/CheckTwoButton.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  max-width: 430px;
+`
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 30px;
+  padding: 20px;
+`
+
+const CommonButton = styled.button`
+  width: 100%;
+  height: 60px;
+  font-size: 16px;
+  border: none;
+  color: white;
+  font-weight: 700;
+  border-radius: 20px;
+`
+const FirstButton = styled(CommonButton)`
+  background-color: #CDDEFF;
+  color: #0057FF;
+  margin-right: 5px;
+  cursor: pointer;
+`
+const SecondButton = styled(CommonButton)`
+  background-color: ${(props) => props.theme.mainBlue};
+  margin-left: 5px;
+  cursor: pointer;
+`
+
+
+const SubText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  line-height: 22px;
+
+  font-size: 15px;
+  color: #A9B0B9;
+  margin: 10px 39px;
+`
+
+
+function CheckTwoButton({firstButton, secondButton, subText}) {
+
+  return (
+    <Wrapper>
+      <SubText>{subText}</SubText>
+      <ButtonWrapper>
+        <FirstButton onClick={firstButton.onClick}>{firstButton.text}</FirstButton>
+        <SecondButton onClick={secondButton.onClick}>{secondButton.text}</SecondButton>
+      </ButtonWrapper>
+    </Wrapper>
+  )
+}
+
+export default CheckTwoButton
--- src/views/Result.js
+++ src/views/Result.js
@@ -2,8 +2,8 @@ import React, {useContext} from 'react'
 import styled from 'styled-components'
 import Lottie from 'lottie-react'
 import {mopedRiderLottie} from '../images/assets'
-import CheckButton from '../components/CheckButton'
 import GlobalContext from '../contexts/store'
+import CheckTwoButton from '../components/CheckTwoButton'
 
 const Wrapper = styled.div`
   padding: 30px;
@@ -20,62 +20,6 @@ const SubTitle = styled.div`
   margin-top: 20px;
 `
 
-const Footer = styled.div`
-  position: fixed;
-  bottom: 0;
-
-  width: 430px;
-  padding: 20px;
-`
-
-const ContactGuide = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-
-  font-size: 15px;
-  color: #A9B0B9;
-  margin-bottom: 30px;
-`
-
-const ButtonWT = styled.div`
-  display: flex;
-  font-weight: 700;
-  font-size: 16px;
-  color: white;
-
-  display: flex;
-  justify-content: center;
-`
-
-const ContactButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  border: none;
-  height: 60px;
-  border-radius: 20px;
-  background-color: #D5E0F7;
-  color: #2662D5;
-
-  margin-right: 10px;
-  cursor: pointer;
-`
-
-const RankingButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  border: none;
-  height: 60px;
-  border-radius: 20px;
-  background-color: ${(props) => props.theme.mainBlue};
-  cursor: pointer;
-`
-
 function Result() {
   const {setState} = useContext(GlobalContext);
 
@@ -84,17 +28,17 @@ function Result() {
       <Wrapper>
         <Title>랭킹에 반영되면 <br/> 알려드릴게요</Title>
         <SubTitle>등록하신 전화번호로 알림이 발송될 예정이에요.</SubTitle>
-        <Lottie animationData={mopedRiderLottie} style={{marginTop: '60px'}}/>
+        <Lottie animationData={mopedRiderLottie}/>
       </Wrapper>
 
-      <Footer>
-        <ContactGuide>혹시 서비스에 궁금하거나 제안할 내용이 있으신가요?<br></br>아래 문의하기 버튼을 눌러 연락주세요!</ContactGuide>
-        <ButtonWT>
-          <ContactButton><a target={'_blank'} href={'https://open.kakao.com/o/s32uFZqe'}>문의하기</a></ContactButton>
-          <RankingButton onClick={() => setState({ page: 0 })}>랭킹으로 돌아가기</RankingButton>
-        </ButtonWT>
-      </Footer>
-      {/* <CheckButton text={'랭킹으로 돌아가기'} enabled={true} onClick={() => setState({page: 0})}/> */}
+      <CheckTwoButton
+        firstButton={{
+          onClick: () => console.log('first'),
+          text: <a target={'_blank'} href={'https://open.kakao.com/o/s32uFZqe'}>문의하기</a>
+        }}
+        secondButton={{onClick: () => setState({page: 0}), text: '랭킹으로 돌아가기'}}
+        subText={<>혹시 서비스에 궁금하거나 제안할 내용이 있으신가요?<br></br>아래 문의하기 버튼을 눌러 연락주세요!</>}
+      />
     </>
   )
 }
